64ecb378e37d63800ba54699ee6cc69864c5db90
1cexplains101-cmyk <1cexplains101@gmail.com>
2025-09-25 12:16:33 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+/* ---------- Word list ---------- */
+const baseWords = [
+ {word:"compassionate",definition:"Showing sympathy and concern for others; kind and caring."},
+ {word:"technologist",definition:"A person skilled in the practical application of technology."},
+ {word:"enterprising",definition:"Showing initiative and resourcefulness; ready to undertake new projects."},
+ {word:"vor
